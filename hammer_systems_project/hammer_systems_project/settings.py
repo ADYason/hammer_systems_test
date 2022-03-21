19a6235b5d48093e3d2106c771d69f6f0211e861
Goryainov Yaroslav <goryainov.yaroslav@gmail.com>
2022-03-21 23:09:27 +0300

    issues with docker-compose

--- hammer_systems_project/hammer_systems_project/settings.py
+++ hammer_systems_project/hammer_systems_project/settings.py
@@ -1,9 +1,13 @@
 import os
 from datetime import timedelta
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = 'w6j$d&%yhdqn%ph4@z&rg1p-09j5@u!w=z3cxx895!%$#kd+*i'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
@@ -58,8 +62,12 @@ WSGI_APPLICATION = 'hammer_systems_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.getenv('DB_ENGINE'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', default='db'),
+        'PORT': os.getenv('DB_PORT')
     }
 }
 
